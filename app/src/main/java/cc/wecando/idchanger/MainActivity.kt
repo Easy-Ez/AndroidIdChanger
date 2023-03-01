69ca2e551038780f21fb18438c449dccd1bc2559
sadhu <c.yao@aftership.com>
2023-03-01 22:53:20 +0800

    feat: use Splash Screen API

--- app/src/main/java/cc/wecando/idchanger/MainActivity.kt
+++ app/src/main/java/cc/wecando/idchanger/MainActivity.kt
@@ -8,16 +8,19 @@ import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.view.ViewTreeObserver
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import cc.wecando.idchanger.ui.main.SectionsPagerAdapter
 import cc.wecando.idchanger.databinding.ActivityMainBinding
+import com.topjohnwu.superuser.Shell
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
+         installSplashScreen()
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
