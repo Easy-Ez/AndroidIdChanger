c35b5d5dbf078cc6a543a3bfef21344017ef104e
sadhu <c.yao@aftership.com>
2023-03-03 18:52:21 +0800

    feat: test code

--- app/src/androidTest/java/cc/wecando/idchanger/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/cc/wecando/idchanger/ExampleInstrumentedTest.kt
@@ -1,12 +1,18 @@
 package cc.wecando.idchanger
 
-import androidx.test.platform.app.InstrumentationRegistry
+import android.content.pm.ApplicationInfo
+import android.os.Build
+import android.util.Log
+import android.util.Xml
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert.*
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.lsposed.hiddenapibypass.HiddenApiBypass
+import org.xmlpull.v1.XmlPullParser
+import java.io.InputStream
 
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -20,5 +26,25 @@ class ExampleInstrumentedTest {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("cc.wecando.idchanger", appContext.packageName)
+        val resolvePullParserMethod = HiddenApiBypass.getDeclaredMethod(
+            Xml::class.java, "resolvePullParser",
+            /*, args */
+            InputStream::class.java
+        )
+        Log.i("version", "version:${Build.VERSION.SDK_INT}")
+        val parser = resolvePullParserMethod.invoke(
+            null, appContext.assets.open("settings_ssaid.xml")
+        ) as XmlPullParser
+        var parserEventType = parser.eventType
+        while (parserEventType != XmlPullParser.END_DOCUMENT) {
+            if (parserEventType == XmlPullParser.START_TAG && parser.name.equals("setting")) {
+                val id = parser.getAttributeValue(null, "id")
+                val name = parser.getAttributeValue(null, "name")
+                val packageName = parser.getAttributeValue(null, "package")
+                Log.i("parser", "id:${id},name:${name},packageName:${packageName}")
+            }
+            parserEventType = parser.next()
+        }
+
     }
 }
