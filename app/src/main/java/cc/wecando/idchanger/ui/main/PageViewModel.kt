670f30524090c156f6dd8accdecfbf1110bcbc24
sadhu <c.yao@aftership.com>
2023-03-11 16:30:58 +0800

    feat: new page

--- app/src/main/java/cc/wecando/idchanger/ui/main/PageViewModel.kt
+++ app/src/main/java/cc/wecando/idchanger/ui/main/PageViewModel.kt
@@ -1,15 +1,11 @@
 package cc.wecando.idchanger.ui.main
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Transformations
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.*
 
 class PageViewModel : ViewModel() {
 
     private val _index = MutableLiveData<Int>()
-    val text: LiveData<String> = Transformations.map(_index) {
+    val text: LiveData<String> = _index.map<Int, String> {
         "Hello world from section: $it"
     }
 
