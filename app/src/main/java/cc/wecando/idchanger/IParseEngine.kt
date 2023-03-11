33c23edc314a85f0aa4347cbb0efc2db6c870204
sadhu <c.yao@aftership.com>
2023-03-11 16:28:16 +0800

    feat: xml parse engine

---
+++ app/src/main/java/cc/wecando/idchanger/IParseEngine.kt
@@ -0,0 +1,26 @@
+package cc.wecando.idchanger
+
+import cc.wecando.idchanger.entity.SettingsSecureIdEntity
+import org.xmlpull.v1.XmlPullParser
+import java.io.InputStream
+
+interface IParseEngine {
+    /**
+     * parse content, whatever abx or xml file
+     */
+    suspend fun parse(inputStream: InputStream): List<SettingsSecureIdEntity>
+
+    /**
+     * default method
+     * convert element to SettingsSecureIdEntity
+     */
+    fun parseItem(parser: XmlPullParser) = SettingsSecureIdEntity(
+        id = parser.getAttributeValue(null, "id"),
+        name = parser.getAttributeValue(null, "name"),
+        packageName = parser.getAttributeValue(null, "package"),
+        value = parser.getAttributeValue(null, "value"),
+        defaultValue = parser.getAttributeValue(null, "defaultValue"),
+        defaultSysSet = parser.getAttributeValue(null, "defaultSysSet"),
+        tag = parser.getAttributeValue(null, "tag"),
+    )
+}
