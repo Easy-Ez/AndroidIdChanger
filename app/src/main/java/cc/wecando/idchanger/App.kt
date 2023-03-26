b382a5d7753d7a77da840e02dfbda93d43521b91
sadhu <c.yao@aftership.com>
2023-03-26 18:03:17 +0800

    feat: custom app entrance

---
+++ app/src/main/java/cc/wecando/idchanger/App.kt
@@ -0,0 +1,15 @@
+package cc.wecando.idchanger
+
+import android.app.Application
+
+class App : Application() {
+    companion object {
+        lateinit var instance: App
+            private set
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+    }
+}
